13a02147964ac037bd719a7a12abded25d97ab59
Samantha <cruz.s98319@gmail.com>
2024-02-04 12:30:04 -0800

    Added document.getElementById to addCustomerName

--- DivByZero_dream_pulse/js/ca-scripts.js
+++ DivByZero_dream_pulse/js/ca-scripts.js
@@ -7,9 +7,12 @@ function addCustomerName() {
     title.textContent = customerName;
     
     // <h1 id="tm-header" class="tm-brand text-uppercase"></h1> <!--todo: Add Customer Name-->
+    let header = document.getElementById('tm-header')
+    header.textContent = customerName;
 
     // <h2 id="introheader" class="tm-section-title mb-5 text-uppercase tm-color-primary"></h2> <!--todo: Add Customer Name-->
-
+    let introHeader = document.getElementById('introheader')
+    introHeader.textContent = customerName;
 };
 
 function toggleColors() {
@@ -19,12 +22,13 @@ function toggleColors() {
 
     whiteRectElements.forEach(function(element) {
         element.classList.remove('tm-white-rect')
-        element.classList.add('tm-blue-react')
+        element.classList.add('tm-blue-rect')
     })
 };
 
 function hideElement() {
     //<ul class="nav flex-row tm-social-links">  <!--todo: Hide social links-->
+    // element.style.display = none
 
 };
 
@@ -35,12 +39,20 @@ function hideElement() {
     introPara1.innerText = "Alliance was founded in 1931 by Albert Daniels"
 
     //<p id="intropara2" class="mb-0 tm-color-gray"> <!--todo: Add customer company introduction text about what they lead in-->
+    let introPara2 = document.getElementById('intropara2')
+    introPara2.innerText = "The company is a leader in P.C. Manufacturing"
 
-     //<p id="aboutpara1" id="a" class="mb-5"> <!--todo: Add customer company about text-->
+    //<p id="aboutpara1" id="a" class="mb-5"> <!--todo: Add customer company about text-->
+    let aboutPara1 = document.getElementById('aboutpara1')
+    aboutPara1.innerText = "We deliver the best service this side of creation"
 
     //<p id="aboutpara2"> <!--todo: Add customer company motto text-->
+    let aboutPara2 = document.getElementById('aboutpara2')
+    aboutPara2.innerText = "Our motto is satisfaction guaranteed"
 
     // <p id="addresspara" class="mb-3"> <!--todo: Add customer company phone number text-->
+    let addressPara = document.getElementById('addresspara')
+    addressPara.innerText = "1-800-565-5656"
 
 }; 
 
